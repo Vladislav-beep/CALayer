293df809c0f4d721ab4e94169bb4b453f1409204
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-11-26 18:00:34 +0300

    CAGradient

--- CALayer/ViewController.swift
+++ CALayer/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var gradientLayer: CAGradientLayer! {
+        didSet {
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+            gradientLayer.colors = [UIColor.green.cgColor, UIColor.gray.cgColor]
+            gradientLayer.locations = [0, 0.8, 1]
+        }
     }
 
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = imageView.frame.height / 2
+            imageView.backgroundColor = .gray
+            imageView.image = UIImage(named: "prog")
+            imageView.layer.borderWidth = 10
+            imageView.layer.borderColor = UIColor.gray.cgColor
+        }
+    }
+    
+    @IBOutlet weak var button: UIButton! {
+        didSet {
+            button.layer.shadowOffset = CGSize(width: 0, height: 5)
+            button.layer.shadowOpacity = 0.5
+            button.layer.shadowRadius = 5
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        gradientLayer = CAGradientLayer()
+        view.layer.insertSublayer(gradientLayer, at: 0)
 
+    }
+
+    override func viewDidLayoutSubviews() {
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.bounds.size.width, height: view.bounds.size.height)
+    }
 }
 
